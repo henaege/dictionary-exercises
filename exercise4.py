57c88f05cbdbe99cb03956355dfe495c109b62f6
Drew Tolliver <dtolliver@Drews-MacBook-Pro.local>
2017-04-21 09:58:50 -0400

    add exercise 4

--- exercise4.py
+++ exercise4.py
@@ -1,9 +1,13 @@
 def word_histogram(paragraph):
+    para_list = paragraph.split(" ")
     histo = {}
-    for i in word:
+    for i in para_list:
         if not histo.has_key(i):
             histo[i] = 1
         else:
             histo[i] += 1
             
     print(histo)
+
+string_in = raw_input("Paragraph: ")
+word_histogram(string_in)
